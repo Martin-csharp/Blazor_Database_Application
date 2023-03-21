26f4ca57eff3b3f32ebfeb29fd7150e9e2ead53f
marti <marti@MARTIN-PC>
2023-03-21 14:19:39 +0100

    Add project files.

---
+++ Blazor_Database_Application/Data/Models/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace Blazor_Database_Application.Data.Models
+{
+    public class Role
+    {
+        public string Id       { get; set; } = String.Empty;
+        public string RoleName { get; set; } = String.Empty;
+    }
+}
---
+++ Blazor_Database_Application/Data/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blazor_Database_Application.Data.Models
+{
+    public class User
+    {
+        [Required]
+        [EmailAddress]
+        public string LoginName { get; set; } = String.Empty;
+
+        [Required]
+        public string UserRole { get; set; } = String.Empty;
+    }
+}
